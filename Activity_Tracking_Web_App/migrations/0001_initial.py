2243a544d9d71163418e3984e61efdf49a3d2ed7
cinderellab <cinderela80@proton.me>
2021-01-23 19:59:33 +0000

    Version 0.9.5

--- Activity_Tracking_Web_App/migrations/0001_initial.py
+++ Activity_Tracking_Web_App/migrations/0001_initial.py
@@ -26,4 +26,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('model_pic', models.ImageField(blank=True, null=True, upload_to=Activity_Tracking_Web_App.models.test_content_file_name)),
-  
+            ],
+        ),
+        migrations.CreateModel(
+            name='Picture',
+            fields=[
+                ('id', models.AutoField
