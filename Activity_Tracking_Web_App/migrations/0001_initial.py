d2c6e3a7a09b8e22c56f8d36991406a122763c59
cinderellab <cinderela80@proton.me>
2021-02-20 07:28:50 +0000

    add example app

--- Activity_Tracking_Web_App/migrations/0001_initial.py
+++ Activity_Tracking_Web_App/migrations/0001_initial.py
@@ -39,4 +39,5 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Activity',
             fields=[
-                ('id'
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('date', models.DateField(default=datetime.datetime.now))
