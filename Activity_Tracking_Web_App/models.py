202ff1ab0898ffb7b002f9be27909378742a3c3e
cinderellab <cinderela80@proton.me>
2021-03-13 07:08:00 +0000

    brlocks/lglocks: turn into functions

--- Activity_Tracking_Web_App/models.py
+++ Activity_Tracking_Web_App/models.py
@@ -2,4 +2,12 @@ from django.db import models
 from datetime import datetime
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.na
