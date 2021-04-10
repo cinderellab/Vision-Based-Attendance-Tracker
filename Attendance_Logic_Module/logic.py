06452f8ba15b74de2400255f39789e108faa813b
cinderellab <cinderela80@proton.me>
2021-04-10 12:54:17 +0000

    Delete cache.properties

--- Attendance_Logic_Module/logic.py
+++ Attendance_Logic_Module/logic.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 
 for x in range(1, 5):
-  globals()['data%s' % x] = pd.read_csv('test' + str(x) + '.csv', e
+  globals()['data%s' % x] = pd.read_csv('test' + str(x) + '.csv', encoding ="latin1")
+# Concatanete them
+df = pd.concat([data1, data2 ,data3, data4])
+df = df.sort_values(['employee_name' , 'timestamp', 'camera_id
