ff320aa68e07809fd421969a51cac063c805ec3a
cinderellab <cinderela80@proton.me>
2021-04-15 15:18:13 +0000

    Publish Canary

--- Attendance_Logic_Module/logic.py
+++ Attendance_Logic_Module/logic.py
@@ -6,4 +6,11 @@ for x in range(1, 5):
   globals()['data%s' % x] = pd.read_csv('test' + str(x) + '.csv', encoding ="latin1")
 # Concatanete them
 df = pd.concat([data1, data2 ,data3, data4])
-df = df.sort_values(['employee_name' , 'timestamp', 'camera_id
+df = df.sort_values(['employee_name' , 'timestamp', 'camera_id'])
+df['unified_id'] = df['camera_id']
+cols = df.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+df = df[cols]
+
+df['unified_id'] = df['unified_id'].replace(2, 1)
+df['unified_i
