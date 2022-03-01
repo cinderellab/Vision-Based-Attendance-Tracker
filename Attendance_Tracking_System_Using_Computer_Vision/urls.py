c82e572925b7323dfb45b89b2ef697d4842155f5
cinderellab <cinderela80@proton.me>
2022-03-01 21:05:04 +0000

    empty message

--- Attendance_Tracking_System_Using_Computer_Vision/urls.py
+++ Attendance_Tracking_System_Using_Computer_Vision/urls.py
@@ -1,3 +1,8 @@
 """Attendance_Tracking_System_Using_Computer_Vision URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatter
