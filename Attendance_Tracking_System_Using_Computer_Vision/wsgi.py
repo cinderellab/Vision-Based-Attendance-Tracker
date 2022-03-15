a65a10d3421875556fcaf84563a1e36116b239d0
cinderellab <cinderela80@proton.me>
2022-03-15 07:27:39 +0000

    Added all files.

--- Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
+++ Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
@@ -1,4 +1,7 @@
 """
 WSGI config for Attendance_Tracking_System_Using_Computer_Vision project.
 
-It exposes the WS
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deploym
