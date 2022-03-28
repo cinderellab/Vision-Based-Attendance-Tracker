5b87e26e49ead2e254a004ffb1c449ba491235b2
cinderellab <cinderela80@proton.me>
2022-03-28 20:14:03 +0000

    Update admin.html

--- Computer_Vision_Module/Adding_New_Person_And_Training.py
+++ Computer_Vision_Module/Adding_New_Person_And_Training.py
@@ -2,4 +2,7 @@ import numpy as np
 from numpy import load
 from numpy import asarray
 from numpy import savez_compressed
-from
+from .Variables import EMBEDDINGS_PATH,model,PATH_TO_ADDED_PERSON_FOLDER
+from .Load_Dataset import load_dataset
+from .Get_Embeddings import get_embedding
+from .SVM_Classi
