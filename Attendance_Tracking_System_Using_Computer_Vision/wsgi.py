8cd57cf37ba80d093d4bda7eaf41bc020635f3ee
cinderellab <cinderela80@proton.me>
2022-03-28 12:48:54 +0000

    merge with develop

--- Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
+++ Attendance_Tracking_System_Using_Computer_Vision/wsgi.py
@@ -11,4 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Attendance_Tracking_System_Using_Computer_Vision.se
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Attendance_Tracking_System_Using_Computer_Vision.settings')
+
+application = get_wsgi_application()
---
+++ Computer_Vision_Module/Adding_New_Person_And_Training.py
@@ -0,0 +1,5 @@
+import numpy as np
+from numpy import load
+from numpy import asarray
+from numpy import savez_compressed
+from
