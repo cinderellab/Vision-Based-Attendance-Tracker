cbf17075436e612dadf03810b43766fec262a1d6
cinderellab <cinderela80@proton.me>
2022-04-07 10:33:22 +0000

    Fixed docblocks

--- Computer_Vision_Module/Adding_New_Person_And_Training.py
+++ Computer_Vision_Module/Adding_New_Person_And_Training.py
@@ -5,4 +5,11 @@ from numpy import savez_compressed
 from .Variables import EMBEDDINGS_PATH,model,PATH_TO_ADDED_PERSON_FOLDER
 from .Load_Dataset import load_dataset
 from .Get_Embeddings import get_embedding
-from .SVM_Classi
+from .SVM_Classifier import svm_train
+
+def add_person():
+  '''
+  Gets Images Embeddings of new person and concatenate them with the old Embeddings
+  '''
+
+  #
