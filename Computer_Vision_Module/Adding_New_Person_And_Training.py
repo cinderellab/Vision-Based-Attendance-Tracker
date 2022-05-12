996b7effdf926f34b502a47b91697924634b209b
cinderellab <cinderela80@proton.me>
2022-05-12 13:38:43 +0000

    Update kernel headers

--- Computer_Vision_Module/Adding_New_Person_And_Training.py
+++ Computer_Vision_Module/Adding_New_Person_And_Training.py
@@ -27,4 +27,6 @@ def add_person():
 
 
   data = load( EMBEDDINGS_PATH + '/Embeddings-dataset.npz' )
-  old_embeddings , old_labels = data['arr_0'] 
+  old_embeddings , old_labels = data['arr_0'] , data['arr_1']
+  print("OLD EMBEDDINGS:", old_embeddings.shape ,"OLD LABELS:" ,old_labels.shape)
+  new_embeddings , new_labels = newTrainX_Images,tr
