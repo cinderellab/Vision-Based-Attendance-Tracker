4ab283711c4e42e2bec80fd7af59d350b861b339
cinderellab <cinderela80@proton.me>
2022-05-25 18:58:09 +0000

    money

--- Computer_Vision_Module/Get_Embeddings.py
+++ Computer_Vision_Module/Get_Embeddings.py
@@ -2,4 +2,11 @@ import tensorflow as tf
 global graph
 from numpy import expand_dims
 
-graph = tf.get_d
+graph = tf.get_default_graph()
+
+
+def get_embedding(model, face_pixels):
+  '''
+  GET EMBEDDINGS FOR ONLY ONE FACE
+  '''
+  print("Shape of image" ,face_pixels.sha
