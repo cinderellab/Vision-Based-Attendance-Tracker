c424668c90eb1d3ce08334b5e0e977b2ff6a64a6
cinderellab <cinderela80@proton.me>
2022-06-13 21:14:53 +0000

    Added CNAME

--- Computer_Vision_Module/Get_Embeddings.py
+++ Computer_Vision_Module/Get_Embeddings.py
@@ -13,4 +13,9 @@ def get_embedding(model, face_pixels):
   face_pixels = face_pixels.astype('float32') # scale pixel values
   mean, std = face_pixels.mean(), face_pixels.std() # standardize pixel values across channels (global)
   face_pixels = (face_pixels - mean) / std
-  samples = expand_dims(face_pixels, axis=0) # transform face into o
+  samples = expand_dims(face_pixels, axis=0) # transform face into one sample
+  print("Shape of normalized image" ,samples.shape )
+  
+  with graph.as_default():
+    yhat = model.predict(samples) # make prediction to get embedding
+    print(
