4d10d4954dcbf87232ce315ab024dc61b5a6a101
cinderellab <cinderela80@proton.me>
2022-06-14 10:45:11 +0000

    const correctness

--- Computer_Vision_Module/Load_Dataset.py
+++ Computer_Vision_Module/Load_Dataset.py
@@ -2,3 +2,9 @@ from .Load_Faces import load_faces
 from .Variables import EMPLOYEES_NAMES,EMBEDDINGS_PATH
 
 import os
+import numpy as np
+from numpy import savez_compressed
+from numpy import asarray
+from numpy import load
+from numpy import expand_dims
+from nump
