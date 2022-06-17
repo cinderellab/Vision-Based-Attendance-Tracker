9076ae8f644e2608fe2989ea9a6fb57a1ca4e33a
cinderellab <cinderela80@proton.me>
2022-06-17 13:38:09 +0000

    Fix README links

--- Computer_Vision_Module/Load_Dataset.py
+++ Computer_Vision_Module/Load_Dataset.py
@@ -7,4 +7,12 @@ from numpy import savez_compressed
 from numpy import asarray
 from numpy import load
 from numpy import expand_dims
-from nump
+from numpy import asarray
+from numpy import savez_compressed
+from numpy import load
+from os.path import isdir
+from os import listdir
+
+
+
+# load a dataset that contains 
