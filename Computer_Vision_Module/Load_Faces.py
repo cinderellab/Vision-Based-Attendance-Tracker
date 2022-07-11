80bac64aa31c414c9c67d4b317d8cc14845551c9
cinderellab <cinderela80@proton.me>
2022-07-11 12:53:44 +0000

    fixing styling

--- Computer_Vision_Module/Load_Faces.py
+++ Computer_Vision_Module/Load_Faces.py
@@ -8,4 +8,8 @@ from numpy import load
 from .Extract_Faces import extract_face
 
 
-# load imag
+# load images and extract faces for all images in a directory
+def load_faces(directory):
+  '''
+  THIS FUNCTION TAKES DIRECTORY OF (ONE) PERSON IMAGES AS INPUT
+  OUTPUTS A FAC
