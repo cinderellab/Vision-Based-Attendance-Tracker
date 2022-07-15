41a071cf1cba5fdef057a85ae70d23f805c444af
cinderellab <cinderela80@proton.me>
2022-07-15 16:57:18 +0000

    Added bootstrap

--- Computer_Vision_Module/Model_Training.py
+++ Computer_Vision_Module/Model_Training.py
@@ -1,2 +1,6 @@
 from .Load_Dataset import load_dataset
-from .Get_Embedd
+from .Get_Embeddings import get_embedding
+from .SVM_Classifier import svm_train
+from .Variables import model,TRAINING_IMAGES_PATH,EMBEDDINGS_PATH
+import sys
+impor
