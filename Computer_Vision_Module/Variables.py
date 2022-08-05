5b0fc35942d7bff4d049b762aecbd63506148f60
cinderellab <cinderela80@proton.me>
2022-08-05 16:51:44 +0000

    Update sesiis.html

--- Computer_Vision_Module/Variables.py
+++ Computer_Vision_Module/Variables.py
@@ -15,4 +15,13 @@ SVM_MODEL_PATH = os.getcwd()
 
 
 # Create Folders/Classes under photos dir in your Django Project
-TRAINING_IMAGES_PATH =
+TRAINING_IMAGES_PATH = os.getcwd() + '\\..\\media\\photos' 
+
+
+
+# Path of facenet_keras.h5 file
+PATH_TO_FACENET_MODEL = os.getcwd()
+FACENET_MODEL = PATH_TO_FACENET_MODEL + '\\facenet_keras.h5'
+
+
+if o
